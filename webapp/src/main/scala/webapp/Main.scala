db5dafeb37cdabd23dc3d73106bfe0149b9d0e57
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-01-27 19:33:45 +0100

    Simplify

--- webapp/src/main/scala/webapp/Main.scala
+++ webapp/src/main/scala/webapp/Main.scala
@@ -11,36 +11,9 @@ import colibri.Subject
 
 object Main {
   def main(args: Array[String]): Unit =
-    Outwatch.renderInto[SyncIO]("#app", app).unsafeRunSync()
+    Outwatch.renderInto[SyncIO]("body", app).unsafeRunSync()
 
-  def app = div(
-    h1("Hello World!"),
-    counter,
-    inputField,
-  )
-
-  def counter = SyncIO {
-    // https://outwatch.github.io/docs/readme.html#example-counter
-    val number = Subject.behavior(0)
-    div(
-      button("+", onClick(number.map(_ + 1)) --> number, marginRight := "10px"),
-      number,
-    )
+  def app = {
+    a("Hello World!")
   }
-
-  def inputField = SyncIO {
-    // https://outwatch.github.io/docs/readme.html#example-input-field
-    val text = Subject.behavior("")
-    div(
-      input(
-        tpe := "text",
-        value <-- text,
-        onInput.value --> text,
-      ),
-      button("clear", onClick.as("") --> text),
-      div("text: ", text),
-      div("length: ", text.map(_.length)),
-    )
-  }
-
 }
